e07668cc7797f1dbc40efa2922b3ef38f5578c5c
mohamedmousa95 <mohamedmousa95@yahoo.com>
2018-06-03 18:05:29 +0200

    final commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 'use strict';
+//imports the required modules
 import React, { Component } from 'react';
 import {
   StyleSheet,
@@ -13,17 +14,29 @@ import {
   Dimensions,
 } from 'react-native';
 
-const { width, height } = Dimensions.get('window'); //getting dimensions of the device
+//getting dimensions of the device
+const { width, height } = Dimensions.get('window'); 
 
+//setting the size of images according to the device size
 const equalWidth = width /2; 
-const equalHeight = height /3;
-const equalH = equalHeight/2;
-const url = 'https://demo6539122.mockable.io/images';
-const largeMargin = 10;
+const equalHeight = height /3; 
+const equalH = equalHeight/2; 
+
+//url for the json file request
+const url = 'https://demo6539122.mockable.io/images'; 
+
+//setting margin between images so there can be some space
+const largeMargin = 10; 
 const smallMargin = 5;
+
+//array holding the images urls returned from the fetch query
 var newArr = [];
 
-
+//item one in the flatlist is holing three images that are shown in
+//a specific orientation layout using flex ratios component, this item 
+//is getting its data from newArr variable and each newArr cell (index)
+//is holding an array of three images urls that are requested to be displayed
+//inside this list item
 class ListItem1 extends React.PureComponent {
 	  render() {
 	    const item = this.props.item;
@@ -39,6 +52,13 @@ class ListItem1 extends React.PureComponent {
 	  }
  }
 
+//item two in the flatlist is holing three images that are shown in
+//a specific orientation layout using flex ratios component, this item 
+//is getting its data from newArr variable and each newArr cell (index)
+//is holding an array of three images urls that are requested to be displayed
+//inside this list item, this item is different from the previous one in just
+//the orientaion of the images so that they are shown in somehow different 
+//order (large image in the left instead of the right)
 class ListItem2 extends React.PureComponent {
 	render() {
 		const item = this.props.item;
@@ -54,6 +74,8 @@ class ListItem2 extends React.PureComponent {
 	}
 }
 
+//item three in the flatlist is holding one image that is filling the screen
+//width
 class ListItem3 extends React.PureComponent {
   render() {
     const item = this.props.item;
@@ -65,8 +87,13 @@ class ListItem3 extends React.PureComponent {
   }
 }
 
+//defines the component that represents the UI
 export default class App extends Component<{}> {
 
+    //class holding three states :- 1) isloading responsible for checking
+    //the spinner state. 2) datasource holds the newArr variable data which
+    //is passed to the Flatlist's data component. 3) message which shown 
+    //if error happens to diplay it.
 	constructor(props) {
 		super(props);
 		 this.state = {
@@ -77,6 +104,10 @@ export default class App extends Component<{}> {
 		this._executeQuery(url);
 	}
 
+   //changing the response so that it becomes in 2d array instead of 1d array
+   //so that an index or cell containg multiple images can be passed as a one
+   //item to the list item component, also set the isloading state to false
+   //so that the spinner stops 
   _handleResponse = (response) => {
     var i=0;
 	while(response.length!=0){
@@ -91,6 +122,8 @@ export default class App extends Component<{}> {
     this.setState({ isLoading: false,  dataSource:  newArr});
   };
   
+  //fetching the query, parsing it to json and passing it to the _handleResponse
+  //method which convert the response to be in 2d array
   _executeQuery = (query) => {
   fetch(query)
   .then(response => response.json())
@@ -103,8 +136,11 @@ export default class App extends Component<{}> {
    );
   };
 
+  //return index of the flatlist item
   _keyExtractor = (item, index) => index;
 
+  //check for the row index of the flat list item so that
+  //different views will be returned accordingly
   _renderItem = ({item, index}) => {
 	  	if(index%3==0 && newArr[index].length>=3){
 		    return(
@@ -129,6 +165,9 @@ export default class App extends Component<{}> {
 		}
    };
 
+  //shows a spinner while waiting for the request to finish 
+  //shows flatlist containg the images requested or a message if 
+  //something went wrong
   render() {
 	    const spinner = this.state.isLoading ?
 	    <View style={styles.spinner}><ActivityIndicator size='large'/></View>: null;
@@ -145,7 +184,7 @@ export default class App extends Component<{}> {
    }
 }
 
-
+//creates a style object that controls the component’s layout and appearance
 const styles = StyleSheet.create({
   container: {
     flex: 1,
